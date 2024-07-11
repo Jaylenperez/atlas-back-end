6b5f95fd8266641621eea360d658c06a82dfc761
jaylenperez <jaylenestella24@gmail.com>
2024-07-11 13:47:18 -0500

    i dont really know

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -58,7 +58,7 @@ def employee_todo_list(employee_id):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 2-export_to_JSON.py <employee_id>")
     else:
         try:
             employee_id = int(sys.argv[1])
